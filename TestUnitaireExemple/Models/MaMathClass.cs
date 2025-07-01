3738f9a96cf1341ae26b5e5f02c4000e1ee81838
michael person <michael.person@cognitic.be>
2025-07-01 11:05:15 +0200

    Tentative de refactorisation

--- TestUnitaireExemple/Models/MaMathClass.cs
+++ TestUnitaireExemple/Models/MaMathClass.cs
@@ -11,11 +11,22 @@ namespace TestUnitaireExemple.Models
     {
         public static int AddPositiveNumber(int a, int b)
         {
-            if (a == int.MaxValue) throw new TooLargeNumberException();
-            if (b == int.MaxValue) throw new TooLargeNumberException();
-            if (a < 0) throw new NegativeNumberException();
-            if (b < 0) throw new NegativeNumberException();
+            //if (a == int.MaxValue) throw new TooLargeNumberException();
+            //if (b == int.MaxValue) throw new TooLargeNumberException();
+            //if (a < 0) throw new NegativeNumberException();
+            //if (b < 0) throw new NegativeNumberException();
+            Exception? e = CheckNumbers(a, b);
+            if (e != null) throw e;
             return a +b;
+        }
+
+        private static Exception? CheckNumbers(int a, int b)
+        {
+            if (a != int.MaxValue && b != int.MaxValue)
+                return new TooLargeNumberException();
+            if (a > 0 && b > 0)
+                return new NegativeNumberException() ;
+            return null;
         }
     }
 }
